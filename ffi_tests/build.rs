705994f285c1343ceede596b01b827c35e229b5d
vladimirovmm <vladimirov.m.m@mail.ru>
2026-05-29 16:58:17 +0700

    refactor: Удален старый код

--- ffi_tests/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    println!("cargo:rustc-link-search=native=target/debug");
-    println!("cargo:rustc-link-lib=fs4me_local");
-
-    // Настраиваем RPATH: указываем, где искать .so при запуске бинарника
-    // $ORIGIN означает «в той же директории, где находится бинарник»
-    println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
-}
